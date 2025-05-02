0a6d7c614e3f02deaedc3745698b2437f5ee6e94
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-05-02 17:53:15 +0200

    Added HTML, CSS, JS

---
+++ index.js
@@ -0,0 +1,101 @@
+import { tweetsData } from "./data.js";
+
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.heart) {
+    handleLikeClick(e.target.dataset.heart);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  }
+});
+
+function handleLikeClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweets) {
+    if (tweets.uuid === tweetId) {
+      return tweets;
+    }
+  })[0];
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  render();
+}
+
+function handleRetweetClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    if (tweet.uuid === tweetId) {
+      return tweet;
+    }
+  })[0];
+
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+  render();
+}
+
+tweetBtn.addEventListener("click", function () {
+  //   console.log(tweetInput.value);
+});
+
+function getFeedHtml() {
+  let feedHtml = "";
+
+  tweetsData.forEach(function (tweet) {
+    let likeIconClass = "";
+    let retweetIconClass = "";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    }
+
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
+
+    feedHtml += `
+        <div class="tweet">
+        <div class="tweet-inner" id="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                        ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        <i class="fa-solid fa-heart ${likeIconClass}" data-heart="${tweet.uuid}"></i>
+                        ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
+                        ${tweet.retweets}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+        `;
+  });
+
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
+
+render();
