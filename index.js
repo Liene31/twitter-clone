f0535bf2af9253c5a7524a381518f761ab79139b
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-05-04 14:54:56 +0200

    Updated JS

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { tweetsData } from "./data.js";
-
-const tweetInput = document.getElementById("tweet-input");
-const tweetBtn = document.getElementById("tweet-btn");
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 document.addEventListener("click", function (e) {
   if (e.target.dataset.heart) {
@@ -10,6 +8,8 @@ document.addEventListener("click", function (e) {
     handleRetweetClick(e.target.dataset.retweet);
   } else if (e.target.dataset.reply) {
     handleReplyClick(e.target.dataset.reply);
+  } else if (e.target) {
+    handleTweetBtnClick();
   }
 });
 
@@ -51,9 +51,25 @@ function handleReplyClick(replyId) {
   document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
 }
 
-tweetBtn.addEventListener("click", function () {
-  //   console.log(tweetInput.value);
-});
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById("tweet-input");
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba 🌟`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: `${tweetInput.value}`,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+
+    render();
+    tweetInput.value = "";
+  }
+}
 
 function getFeedHtml() {
   let feedHtml = "";
@@ -126,3 +142,15 @@ function render() {
 }
 
 render();
+
+/*
+Challenge:
+1. No empty tweets!
+2. Clear the textarea after tweeting!
+*/
+
+/*
+Challenge:
+3. We could improve index.js by moving one line
+   of code to a better position. Find it and move it!
+*/
